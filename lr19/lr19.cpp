eb4f33c8465123152cea3d03a6c478bb03efec91
Damir <Damir@DESKTOP-ELD43TT>
2024-05-30 02:21:43 +0300

    Добавьте файлы проекта.

---
+++ lr19/lr19.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+template <typename T>
+void power(T x, T& square, T& cube, T& fourthPower) {
+	square = x * x;
+	cube = square * x;
+	fourthPower = cube * x;
+}
+
+int main() {
+	int x = 5;
+	int square, cube, fourthPower;
+
+	power(x, square, cube, fourthPower);
+
+	cout << "Number: " << x << endl;
+	cout << "Square: " << square << endl;
+	cout << "Cube: " << cube << endl;
+	cout << "Fourth power: " << fourthPower << endl;
+
+	return 0;
+}
